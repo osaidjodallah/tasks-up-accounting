2c4dffb88c0eb583bbfbcaaedf391c63013b7b95
osaidjodallah <osaidjodallah@users.noreply.github.com>
2026-04-13 14:47:47 +0300

    Restructure: promote accounting program to main page and remove marketing form

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'marketing-accounting-v1';
-const ASSETS = ['./accounting.html', './manifest.json'];
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./accounting.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
